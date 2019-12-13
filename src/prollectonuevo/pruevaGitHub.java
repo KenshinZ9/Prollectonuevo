21a44736da05e651799365cfb76e4a09d05843d8
DAM_1 <DAM_1@PB07>
2019-12-13 19:45:00 +0100

    suma

--- src/prollectonuevo/pruevaGitHub.java
+++ src/prollectonuevo/pruevaGitHub.java
@@ -1,8 +1,21 @@
 package prollectonuevo;
+
+import java.util.Scanner;
+
 public class pruevaGitHub {
+	public static int numero1=2;
+	public static int numero2=9;
+	
+	
+	public static void suma(int numero1, int numero2) {
+		int total =0;
+		total=numero1+numero2;
+		System.out.println("la suma es "+total);
+	}
 	
 	public static void main(String[] args) {
-		
+		//Scanner teclado = new Scanner(System.in);
+		suma(numero1, numero2);
 	}
 
 }
