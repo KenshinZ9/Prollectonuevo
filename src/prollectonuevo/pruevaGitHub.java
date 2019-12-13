b03a99249ae6aee9c38037509d4172c617d988b7
DAM_1 <DAM_1@PB07>
2019-12-13 19:46:12 +0100

    resta

--- src/prollectonuevo/pruevaGitHub.java
+++ src/prollectonuevo/pruevaGitHub.java
@@ -13,9 +13,16 @@ public class pruevaGitHub {
 		System.out.println("la suma es "+total);
 	}
 	
+	public static void resta(int numero1, int numero2) {
+		int total =0;
+		total=numero1-numero2;
+		System.out.println("la resta es "+total);
+	}
+	
 	public static void main(String[] args) {
 		//Scanner teclado = new Scanner(System.in);
 		suma(numero1, numero2);
+		resta(numero1, numero2);
 	}
 
 }
